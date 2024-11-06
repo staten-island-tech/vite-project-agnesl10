fd2a41827320199e4f1b2a94e6a0945e06d1ec5a
agnesl10 <agnesl10@nycstudents.net>
2024-11-06 12:28:45 -0500

    sdfsdf

--- app/JS/dom.js
+++ app/JS/dom.js
@@ -1,5 +1,8 @@
-DOMSelectors = {
+const DOMSelectors = {
   container: document.querySelector(".container"),
   card: document.querySelector(".card"),
-  title: document.querySelector(""),
+  title: document.querySelector(".title"),
+  relartist: document.querySelector(".relartist"),
+  image: document.querySelector(".image"),
 };
+export { DOMSelectors };
--- app/JS/main.js
+++ app/JS/main.js
@@ -1,21 +1,23 @@
 import "../CSS/style.css";
-import "./dom";
+import { DOMSelectors } from "./dom";
 import { vinylRecords } from "./vinyls";
 
 console.log(vinylRecords);
 
-function createCards(title, artist, genre, year, price, imageUrl) {
+function createCards(record) {
   DOMSelectors.container.insertAdjacentHTML(
     "beforeend",
     `<div class="card">
-    <h1 class="title">${vinylRecords.title}</h1>
+    <h2 class="title">${record.title}</h2>
     <br />
-    <h3 class="relartist">Released in ${vinylRecords.releaseYear} by ${vinylRecords.artist}</h3>
-    <h5 class="genre">${vinylRecords.genre}</h5>
+    <h3 class="relartist">Released in ${record.releaseYear} by ${record.artist}</h3>
+    <h5 class="genre">${record.genre}</h5>
     <br />
-    <img class="image" src="${vinylRecords.imageUrl}" alt="" />
+    <img class="image" src="${record.imageUrl}" alt="" />
     <br />
-    <h4 class="price">${vinylRecords.price}</h4>
+    <h4 class="price">${record.price}</h4>
     </div>`
   );
 }
+
+vinylRecords.forEach((record) => createCards(record));
